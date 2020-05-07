6db7a4191c73b62d5edf85ec30f336630fe44c14
akuntz09 <akuntz09@gmail.com>
2020-05-07 16:33:05 -0400

    Create Edge.java

---
+++ src/Graphs/Edge.java
@@ -0,0 +1,14 @@
+
+public class Edge {
+	Vertex start;
+	Vertex end;
+	int weight;
+	
+	public Edge(Vertex startV, Vertex endV, int inputWeight) {
+		this.start = startV;
+		this.end = endV;
+		this.weight = inputWeight;
+	}
+	
+	
+}
