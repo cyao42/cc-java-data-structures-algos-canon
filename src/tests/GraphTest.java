c2a5ce77973fa5256bce1a7493aeece75521c6b0
Carolyn Yao <cyao@Carolyn-Yaos-MacBook-Pro.local>
2020-05-08 17:38:21 -0400

    add test directory with files

---
+++ src/tests/GraphTest.java
@@ -0,0 +1,28 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GraphTest {
+
+    @Test
+    void addVertex() {
+    }
+
+    @Test
+    void removeVertex() {
+    }
+
+    @Test
+    void addEdge() {
+    }
+
+    @Test
+    void removeEdge() {
+    }
+
+    @Test
+    void getVertexByValue() {
+    }
+}
