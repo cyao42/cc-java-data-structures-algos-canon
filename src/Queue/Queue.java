3b133fcc33df317d3f3711c256b0d0f39e22b13f
alishagrama <agrama@u.rochester.edu>
2020-05-08 17:13:54 -0400

    added queue and stack

---
+++ src/Queue/Queue.java
@@ -0,0 +1,38 @@
+public class Queue {
+
+  public LinkedList queue;
+  public int size;
+
+  public Queue() {
+    this.queue = new LinkedList();
+    this.size = 0;
+  }
+
+  public boolean isEmpty() {
+    return this.size == 0;
+  }
+
+  public void enqueue(String data) {
+    this.queue.addToTail(data);
+    this.size++;
+  }
+
+  public String peek() {
+    if (this.isEmpty()) {
+      return null;
+    } else {
+      return this.queue.head.data;
+    }    
+  }
+
+  public String dequeue() {
+    if (!this.isEmpty()) {
+      String data = this.queue.removeHead();
+      this.size--;
+      return data;
+    } else {
+      throw new Error("Queue is empty!");
+    }
+  }
+
+}
