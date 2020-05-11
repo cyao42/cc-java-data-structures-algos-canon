db210bc7d920452b8ce0b476fc1732fdd79339ba
alishagrama <agrama@u.rochester.edu>
2020-05-11 15:52:24 -0400

    added queue testing

--- src/test/QueueTest.java
+++ src/test/QueueTest.java
@@ -1,78 +1,68 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class QueueTest {
     @Test
-    public void newList() {
-        // Successfully sets head to null
-        LinkedList list = new LinkedList();
-        Node expectedNode = null;
-        Node actualNode = list.head;
-        assertEquals("Head not initialized properly", expectedNode, actualNode);
+    public void newQueue() {
+        // Successfully creates a new queue
+        // this.size set to 0, this.queue is an empty LinkedList
+        Queue q = new Queue();
+        assertEquals("Size not set to 0", 0, q.size);
+        assertEquals("this.queue not initialized to empty LinkedList", null, q.queue.head);
     }
 
     @Test
-    public void addToHeadEmptyList() {
-        // Can successfully add head of an empty list
-        LinkedList list = new LinkedList();
-        list.addToHead("head");
-        assertEquals("Head of empty list not set properly", list.head.data, "head");
+    public void isEmptyEmpty() {
+        // returns true for empty queue
+        Queue q = new Queue();
+        assertEquals("Does not return true for an empty queue", true, q.isEmpty());
     }
 
     @Test
-    public void addToHeadNotEmpty() {
-        // Can successfully add to the head of a non-empty list
-        LinkedList list = new LinkedList();
-        list.addToHead("first");
-        list.addToHead("second");
-        assertEquals("Head of list not updated", list.head.data, "second");
+    public void isEmptyNotEmpty() {
+        // Returns false for a non-empty queue
+        Queue q = new Queue();
+        q.enqueue("first");
+        assertEquals("Does not return false for a non-empty queue", false, q.isEmpty());
     }
 
     @Test
-    public void addToTailEmptyList() {
-        // Can successfully add to tail of an empty list
-        LinkedList list = new LinkedList();
-        list.addToTail("head");
-        assertEquals("Head of empty list not set properly using `.addToTail()`", list.head.data, "head");
+    public void enqueueValid() {
+        // Adds to and updates tail of queue
+        // Size incremented
+        Queue q = new Queue();
+        q.enqueue("first");
+        q.enqueue("second");
+        assertEquals("Does not add to tail of the queue", "second", q.queue.head.getNextNode().data);
+        assertEquals("Does not increment size", q.size, 2);
     }
 
     @Test
-    public void addToTailNotEmpty() {
-        // Can successfully add to the tail of a non-empty list
-        LinkedList list = new LinkedList();
-        list.addToTail("first");
-        list.addToTail("second");
-        assertEquals("Cannot add to tail of the list", list.head.getNextNode().data, "second");
+    public void peekEmpty() {
+        // Returns null
+        Queue q = new Queue();
+        assertEquals("Does not return null for an empty queue", null, q.peek());
     }
 
     @Test
-    public void removeHeadEmptyList() {
-        // Can successfully remove head of a now empty list
-        LinkedList list = new LinkedList();
-        list.addToHead("head");
-        list.removeHead();
-        assertEquals("Cannot remove the head", list.head, null);
+    public void peekNotEmpty() {
+        // Returns but doesn't remove head of queue
+        Queue q = new Queue();
+        q.enqueue("first");
+        assertEquals("Does not return data of first item in the queue", "first", q.peek());
+        assertEquals("Removes head of the queue", "first", q.queue.head.data);
     }
 
     @Test
-    public void removeHeadNotEmpty() {
-        // Can successfully remove to the head of a still non-empty list
-        LinkedList list = new LinkedList();
-        list.addToHead("first");
-        list.addToHead("second");
-        list.removeHead();
-        assertEquals("Head of list not removed", list.head.data, "first");
+    public void dequeueNotEmpty() {
+        // Removes and returns head
+        // Size decremented
+        Queue q = new Queue();
+        q.enqueue("first");
+        q.enqueue("second");
+        assertEquals("Does not return the head of the queue", q.dequeue(), "first");
+        assertEquals("Does not remove the head of the queue", q.queue.head.data, "second");
+        assertEquals("Does not decrement the size of the queue", q.size, 1);
     }
-
-    @Test
-    public void testToString() {
-        // .toString() method correctly prints list
-        LinkedList list = new LinkedList();
-        list.addToHead("first");
-        list.addToHead("second");
-        String expected = "<head> second first <tail>";
-        String actual = list.toString();
-        assertEquals(".toString() method not printing correctly", expected, actual);
-    }
-
+    
 }
