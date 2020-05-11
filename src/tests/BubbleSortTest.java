0e0b4d6fd7927bb8f0ddaf0315225723639cc4fb
Carolyn Yao <cyao42@gmail.com>
2020-05-11 19:24:51 -0400

    bad syntax check

--- src/tests/BubbleSortTest.java
+++ src/tests/BubbleSortTest.java
@@ -9,7 +9,7 @@ public class BubbleSortTest {
     public void sort() {
         BubbleSort bs = new BubbleSort();
         int[] testArray = {64, 34, 25, 12, 22, 11, 90};
-        int[] testArraySorted = {11, 10, 22, 25, 34, 64, 90};
+        int[] testArraySorted = {11, 10, 22, 25, 34, 64, 90}
 
         int[] bubbleSorted = bs.sort(testArray);
         assertArrayEquals("The sort did not return the expected result", testArraySorted, bubbleSorted);
