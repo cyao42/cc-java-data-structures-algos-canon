6bb2784b4616ddaf0e831709b978b2e788c7a1ac
Carolyn Yao <cyao42@gmail.com>
2020-05-14 11:44:21 -0400

    update bubble sort test

--- src/tests/BubbleSortTest.java
+++ src/tests/BubbleSortTest.java
@@ -2,7 +2,7 @@ package tests;
 
 import SortingAlgorithms.BubbleSort;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
 
 public class BubbleSortTest {
 
@@ -10,19 +10,60 @@ public class BubbleSortTest {
     public void sort() {
         BubbleSort bs = new BubbleSort();
         int[] testArray = {64, 34, 25, 12, 22, 11, 90};
-        int[] testArraySorted = {11, 12, 22, 25, 34, 64, 90};
+        int[] expected = {11, 12, 22, 25, 34, 64, 90};
 
-        int[] bubbleSorted = bs.sort(testArray);
-        assertArrayEquals("The sort did not return the expected result", testArraySorted, bubbleSorted);
+        assertArrayEquals(
+                "BubbleSort fails to sort an all-positive array",
+                bs.sort(testArray),
+                expected
+        );
 
         int[] testArrayOneElement = {32};
-        int[] testArrayOneElementSorted = {32};
-        int[] bubbleSortedOneElement = bs.sort(testArrayOneElement);
-        assertArrayEquals("The sort did not return the expected result", testArrayOneElementSorted, bubbleSortedOneElement);
+        expected = new int[]{32};
+        assertArrayEquals(
+                "BubbleSort fails to sort an array with a single element",
+                bs.sort(testArrayOneElement),
+                expected
+        );
 
         int[] testArrayBlank = {};
-        int[] testArrayBlankSorted = {};
-        int[] bubbleSortedBlank = bs.sort(testArrayBlank);
-        assertArrayEquals("The sort did not return the expected result", testArrayBlankSorted, bubbleSortedBlank);
+        expected = new int[]{};
+        assertArrayEquals(
+                "BubbleSort should have returned an empty array as is",
+                bs.sort(testArrayBlank),
+                expected
+        );
+
+        int[] alreadySorted = {1, 2, 3, 4, 5, 6, 7};
+        expected = alreadySorted;
+        assertArrayEquals(
+                "BubbleSort should not change an already sorted array",
+                bs.sort(alreadySorted),
+                expected
+        );
+
+        int[] happyArray = {0, -1, 8, 7, 6, 5};
+        expected = new int[]{-1, 0, 5, 6, 7, 8};
+        assertArrayEquals(
+                "BubbleSort fails to sort a happy array",
+                bs.sort(happyArray),
+                expected
+        );
+
+        int[] arrayWithMultiples = {4, 4, 4, 4, 4, 4, 4, 4};
+        expected = arrayWithMultiples;
+        assertArrayEquals(
+                "BubbleSort should not change an array filled with multiples of the same element",
+                bs.sort(arrayWithMultiples),
+                expected
+        );
+
+        int[] negativeArray = {-4, -50, -9, -100, -20, -3, -2, -44, -13, -25};
+        expected = new int[]{-100, -50, -44, -25, -20, -13, -9, -4, -3, -2};
+        assertArrayEquals(
+                "BubbleSort fails to sort an all-negative array",
+                bs.sort(negativeArray),
+                expected
+        );
     }
 }
