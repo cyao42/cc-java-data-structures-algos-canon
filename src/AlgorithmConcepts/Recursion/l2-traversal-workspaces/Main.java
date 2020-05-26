e961653495b05f1b506a9aea0968a79b9d5fb3c3
Alexander Clark <apclarkva@gmail.com>
2020-05-26 09:55:56 -0400

    Create workspace directories for recursion lessons

---
+++ src/AlgorithmConcepts/Recursion/l2-traversal-workspaces/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+	// Call with java Main.java Factorial.java
+        System.out.println("This will be printed");
+	System.out.println(Factorial.fact(4));
+    }
+}
