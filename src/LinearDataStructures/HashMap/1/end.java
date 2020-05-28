543ddad89ea4feb90520dc05b8c7440d2eb1fe0a
Robert Merrill <robgmerrill@gmail.com>
2020-05-28 09:46:01 -0700

    Exercise 1

---
+++ src/LinearDataStructures/HashMap/1/end.java
@@ -0,0 +1,11 @@
+package LinearDataStructures.HashMap;
+
+public class HashMap {
+
+    public LinkedList[] hashmap;
+
+    public HashMap(int size) {
+        this.hashmap = new LinkedList[size];
+    }
+
+}
