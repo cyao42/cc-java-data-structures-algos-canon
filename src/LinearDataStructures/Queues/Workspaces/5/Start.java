91288f27f54f75bfd4e65a8b39174af8b4df9958
Emma Juliette Brown <60446806+emmajbrown@users.noreply.github.com>
2020-05-27 22:15:55 -0700

    Create Start.java

---
+++ src/LinearDataStructures/Queues/Workspaces/5/Start.java
@@ -0,0 +1,43 @@
+public class Queue {
+
+    public LinkedList queue;
+    public int size;
+    static final int DEFAULT_MAX_SIZE = Integer.MAX_VALUE;
+    public int maxSize;
+    
+    public Queue() {
+        this(DEFAULT_MAX_SIZE);
+    }
+    
+    public Queue(int maxSize) {
+        this.queue = new LinkedList();
+        this.size = 0;
+        this.maxSize = maxSize;
+    }
+    
+    public boolean hasSpace() {
+        return this.size < this.maxSize;
+    }
+    
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+    
+    public void enqueue(String data) {
+        this.queue.addToTail(data);
+        this.size++;
+        System.out.println("Added " + data + "! Queue size is now " + this.size);
+    }
+    
+    public static void main(String[] args) {
+    /*
+        Queue smoothieOrders = new Queue();
+        smoothieOrders.enqueue("strawberry banana");
+        smoothieOrders.enqueue("chocolate peanut butter");
+        System.out.println("Blending the smoothies...");
+        smoothieOrders.dequeue();
+        smoothieOrders.dequeue();
+        System.out.println("All orders are complete! No one is in line.");
+     */
+    }
+}
