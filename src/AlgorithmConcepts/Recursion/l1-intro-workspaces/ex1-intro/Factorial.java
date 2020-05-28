143e5f804d96c99cfa97e19fd544982ba7cdd8ea
Alexander Clark <apclarkva@gmail.com>
2020-05-28 09:49:09 -0400

    Added lesson 1 workspaces

---
+++ src/AlgorithmConcepts/Recursion/l1-intro-workspaces/ex1-intro/Factorial.java
@@ -0,0 +1,18 @@
+public class Factorial {
+    public static int iterativeFactorial(int n) {
+	int result = 1;
+
+	while(n > 0) {
+	    result *= n;
+	    n -= 1;
+	}
+
+        return result;
+    }
+    public static void main(String[] args) {
+	// Set int below
+	
+	// Print int below
+	
+    }
+}
