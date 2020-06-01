7a1cc4d3af221e8d2507b0eb5c11f9c856adb5ca
Emma Juliette Brown <60446806+emmajbrown@users.noreply.github.com>
2020-05-31 22:22:05 -0700

    Update End.java

--- src/LinearDataStructures/Queues/Workspaces/5/End.java
+++ src/LinearDataStructures/Queues/Workspaces/5/End.java
@@ -1,5 +1,5 @@
 public class Queue {
-
+    
     public LinkedList queue;
     public int size;
     static final int DEFAULT_MAX_SIZE = Integer.MAX_VALUE;
@@ -13,14 +13,6 @@ public class Queue {
         this.queue = new LinkedList();
         this.size = 0;
         this.maxSize = maxSize;
-    }
-    
-    public boolean hasSpace() {
-        return this.size < this.maxSize;
-    }
-    
-    public boolean isEmpty() {
-        return this.size == 0;
     }
     
     public void enqueue(String data) {
@@ -40,13 +32,20 @@ public class Queue {
         return this.queue.head.data;
     }
     
+    public boolean hasSpace() {
+        return this.size < this.maxSize;
+    }
+    
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+    
     public static void main(String[] args) {
-        Queue smoothieOrders = new Queue();
-        smoothieOrders.enqueue("strawberry banana");
-        smoothieOrders.enqueue("chocolate peanut butter");
-        System.out.println("Blending the smoothies...");
-        smoothieOrders.dequeue();
-        smoothieOrders.dequeue();
-        System.out.println("All orders are complete!");
+      Queue queueOne = new Queue(25);
+      Queue queueTwo = new Queue(0);
+      System.out.println("queueOne has space for more nodes: " + queueOne.hasSpace());
+      System.out.println("queueTwo has space for more nodes: " + queueTwo.hasSpace());
+      System.out.println("queueOne is empty: " + queueOne.isEmpty());
+      System.out.println("queueTwo is empty: " + queueTwo.isEmpty());
     }
 }
