5449557b2aa7d3bf53c3044023bf1d83eb8539e5
Emma Juliette Brown <60446806+emmajbrown@users.noreply.github.com>
2020-05-31 22:08:17 -0700

    Update End.java

--- src/LinearDataStructures/Queues/Workspaces/4/End.java
+++ src/LinearDataStructures/Queues/Workspaces/4/End.java
@@ -1,5 +1,5 @@
 public class Queue {
-
+    
     public LinkedList queue;
     public int size;
     static final int DEFAULT_MAX_SIZE = Integer.MAX_VALUE;
@@ -14,26 +14,31 @@ public class Queue {
         this.size = 0;
         this.maxSize = maxSize;
     }
-    
-    public boolean hasSpace() {
-        return this.size < this.maxSize;
-    }
-    
-    public boolean isEmpty() {
-        return this.size == 0;
-    }
-    
+
     public void enqueue(String data) {
         this.queue.addToTail(data);
         this.size++;
-        System.out.println("Added " + data + "! Queue size is now " + this.size + ".");
+        System.out.println("Added " + data + "! Queue size is now " + this.size);
+    }
+    
+    public String dequeue() {
+        String data = this.queue.removeHead();
+        this.size--;
+        System.out.println("Removed " + data + "! Queue size is now " + this.size + ".");
+        return data;
+    }
+    
+    public String peek() {
+        return this.queue.head.data;
     }
     
     public static void main(String[] args) {
-        Queue coffeeOrder = new Queue();
-        System.out.println("coffeeOrder queue has " + coffeeOrder.size + " orders.");
-        coffeeOrder.enqueue("latte");
-        coffeeOrder.enqueue("espresso");
-        coffeeOrder.enqueue("cappuccino");
+        Queue smoothieOrders = new Queue();
+        smoothieOrders.enqueue("strawberry banana");
+        smoothieOrders.enqueue("chocolate peanut butter");
+        System.out.println("Blending the smoothies...");
+        smoothieOrders.dequeue();
+        smoothieOrders.dequeue();
+        System.out.println("All orders are complete!");
     }
 }
