9ec4f0fefebb9e3d2db9b3b70870df77d2a15b01
alishagrama <agrama@u.rochester.edu>
2020-06-01 16:18:37 -0400

    finished Node workspaces

--- src/LinearDataStructures/Nodes/Workspaces/Exercise 1/NodeStart.java
+++ src/LinearDataStructures/Nodes/Workspaces/Exercise 1/NodeStart.java
@@ -1,3 +1,6 @@
 public class Node {
-	
+
+	public static void main(String[] args) {
+		
+	}
 }
--- src/LinearDataStructures/Nodes/Workspaces/Exercise 4/NodeStart.java
+++ src/LinearDataStructures/Nodes/Workspaces/Exercise 4/NodeStart.java
@@ -1,17 +1,24 @@
 public class Node {
 
   public String data;
-  public Node next;	
+  private Node next;	
 
   public Node(String data) {
     this.data = data;
     this.next = null;
   }
 
+  public void setNextNode(Node node) {
+    this.next = node;
+  }
+
+  public Node getNextNode() {
+    return this.next;
+  }
+
   public static void main(String[] args) {
-    Node firstNode = new Node("I am a Node!");
-    System.out.println(firstNode.data);
-    System.out.println(firstNode.next);
+    
+
   }
 
 }
