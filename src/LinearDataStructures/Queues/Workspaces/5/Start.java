751b1968765a8419972274f71a715ffe88c32290
Emma Juliette Brown <60446806+emmajbrown@users.noreply.github.com>
2020-05-31 22:19:02 -0700

    Update Start.java

--- src/LinearDataStructures/Queues/Workspaces/5/Start.java
+++ src/LinearDataStructures/Queues/Workspaces/5/Start.java
@@ -1,5 +1,5 @@
 public class Queue {
-
+    
     public LinkedList queue;
     public int size;
     static final int DEFAULT_MAX_SIZE = Integer.MAX_VALUE;
@@ -15,29 +15,31 @@ public class Queue {
         this.maxSize = maxSize;
     }
     
-    public boolean hasSpace() {
-        return this.size < this.maxSize;
-    }
-    
-    public boolean isEmpty() {
-        return this.size == 0;
-    }
-    
     public void enqueue(String data) {
         this.queue.addToTail(data);
         this.size++;
         System.out.println("Added " + data + "! Queue size is now " + this.size);
     }
     
+    public String dequeue() {
+        String data = this.queue.removeHead();
+        this.size--;
+        System.out.println("Removed " + data + "! Queue size is now " + this.size + ".");
+        return data;
+    }
+    
+    public String peek() {
+        return this.queue.head.data;
+    }
+    
     public static void main(String[] args) {
-    /*
-        Queue smoothieOrders = new Queue();
-        smoothieOrders.enqueue("strawberry banana");
-        smoothieOrders.enqueue("chocolate peanut butter");
-        System.out.println("Blending the smoothies...");
-        smoothieOrders.dequeue();
-        smoothieOrders.dequeue();
-        System.out.println("All orders are complete!");
-     */
+      /*
+      Queue queueOne = new Queue(25);
+      Queue queueTwo = new Queue(0);
+      System.out.println("queueOne has space for more nodes: " + queueOne.hasSpace());
+      System.out.println("queueTwo has space for more nodes: " + queueTwo.hasSpace());
+      System.out.println("queueOne is empty: " + queueOne.isEmpty());
+      System.out.println("queueTwo is empty: " + queueTwo.isEmpty());
+      */
     }
 }
