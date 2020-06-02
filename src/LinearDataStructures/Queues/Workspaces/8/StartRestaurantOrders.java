4d164adb9346bd2a4f8acd3a62d4889430a4b69c
Emma Juliette Brown <60446806+emmajbrown@users.noreply.github.com>
2020-06-01 23:28:19 -0700

    Create StartRestaurantOrders.java

---
+++ src/LinearDataStructures/Queues/Workspaces/8/StartRestaurantOrders.java
@@ -0,0 +1,19 @@
+public class RestaurantOrders {
+
+    public Queue headChef;
+    public Queue sousChef;
+    public Queue waitingList;
+
+    public void assign(String[] orders) {
+        for (String order : orders) {
+            try {
+
+            } catch (Error e) {
+
+            }
+
+        }
+        System.out.println("--------------------");
+        System.out.println("You've got just " + waitingList.size + " orders on the waiting list. Keep up the hard work chefs!");
+    }
+}
