39e64ed8bd6c7e9c58e253eb63020677b102b56d
Emma Juliette Brown <60446806+emmajbrown@users.noreply.github.com>
2020-06-15 17:25:57 -0700

    Create End.java

---
+++ src/LinearDataStructures/Stacks/Workspaces/4/End.java
@@ -0,0 +1,45 @@
+public class Stack {
+
+    public LinkedList stack;
+    public int size;
+    static final int DEFAULT_MAX_SIZE = Integer.MAX_VALUE;
+    public int maxSize;
+
+    public Stack() {
+        this(DEFAULT_MAX_SIZE);
+    }
+
+    public Stack(int maxSize) {
+        this.stack = new LinkedList();
+        this.size = 0;
+        this.maxSize = maxSize;
+    }
+
+    public void push(String data) {
+        this.stack.addToHead(data);
+        this.size++;
+        System.out.println("Added " + data + "! Stack size is now " + this.size);
+    }
+
+    // Define pop() below
+    public String pop() {
+        String data = this.stack.removeHead();
+        this.size--;
+        System.out.println("Removed " + data + "! Stack size is now " + this.size);
+        return data;
+    }
+
+    // Define peek() below
+    public String peek() {
+        return this.stack.head.data;
+    }
+
+    public static void main(String[]args) {
+        Stack carsInDriveWay = new Stack();
+        carsInDriveWay.push("van");
+        carsInDriveWay.push("convertible");
+        carsInDriveWay.push("truck");
+        System.out.println("The driveway is full! The " + carsInDriveWay.peek() + " needs to leave first.");
+        carsInDriveWay.pop();
+    }
+}
