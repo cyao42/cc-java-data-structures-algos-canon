caa8d270372a4a635b1f8a5f56967db562dc7cb5
Emma Juliette Brown <60446806+emmajbrown@users.noreply.github.com>
2020-06-16 13:25:25 -0700

    Update End.java

--- src/LinearDataStructures/Stacks/Workspaces/6/End.java
+++ src/LinearDataStructures/Stacks/Workspaces/6/End.java
@@ -54,10 +54,8 @@ public class Stack {
     }
 
     public static void main(String[]args) {
-        /*
         Stack carsInDriveWay = new Stack();
         carsInDriveWay.push("van");
         carsInDriveWay.push("convertible");
-         */
     }
 }
