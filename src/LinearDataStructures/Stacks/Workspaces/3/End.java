7cdee097f4f474847794ed2352c0a7b23080cae0
Emma Juliette Brown <60446806+emmajbrown@users.noreply.github.com>
2020-06-15 17:10:09 -0700

    Create End.java

---
+++ src/LinearDataStructures/Stacks/Workspaces/3/End.java
@@ -0,0 +1,31 @@
+public class Stack {
+
+    public LinkedList stack;
+    public int size;
+    static final int DEFAULT_MAX_SIZE = Integer.MAX_VALUE;
+    public int maxSize;
+
+    public Stack() {
+        this(DEFAULT_MAX_SIZE);
+    }
+
+    public Stack(int maxSize) {
+        this.stack = new LinkedList();
+        this.size = 0;
+        this.maxSize = maxSize;
+    }
+
+    // Define push() below
+    public void push(String data) {
+        this.stack.addToHead(data);
+        this.size++;
+        System.out.println("Added " + data + "! Stack size is now " + this.size);
+    }
+
+    public static void main(String[]args) {
+        Stack dishStack = new Stack();
+        dishStack.push("large plate");
+        dishStack.push("small plate");
+        dishStack.push("bowl");
+    }
+}
