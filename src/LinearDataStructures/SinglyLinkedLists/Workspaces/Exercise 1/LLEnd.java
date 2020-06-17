95e1364a0479ebe85367ed615af5c966a3f8e7a8
alishagrama <agrama@u.rochester.edu>
2020-06-17 15:07:59 -0400

    workspaces LinkedList

---
+++ src/LinearDataStructures/SinglyLinkedLists/Workspaces/Exercise 1/LLEnd.java
@@ -0,0 +1,18 @@
+public class LinkedList {
+
+	private Node head;
+
+	public LinkedList() {
+		this.head = null;
+	}
+
+  public void addToHead(String data) {
+    Node newHead = new Node(data);
+    Node currentHead = this.head;
+    this.head = newHead;
+    if (currentHead != null) {
+      this.head.setNextNode(currentHead);
+    }
+  }
+
+}
