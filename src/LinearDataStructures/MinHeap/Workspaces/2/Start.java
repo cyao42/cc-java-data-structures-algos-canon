5bb71c5a9f801b7f5084b10f404c4c04d731dee4
Emma Juliette Brown <60446806+emmajbrown@users.noreply.github.com>
2020-07-12 15:06:37 -0600

    Create Start.java

---
+++ src/LinearDataStructures/MinHeap/Workspaces/2/Start.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class MinHeap {
+    public ArrayList<Integer> heap;
+    public int size;
+
+    // Define MinHeap constructor below
+
+
+    public static void main(String[]args) {
+        // Instantiate a minHeap, assign to minHeap
+
+        // Display contents of minHeap
+    }
+
+}
