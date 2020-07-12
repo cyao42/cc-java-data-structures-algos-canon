1a8c5649b4f24db8df111e2afef351b0cdf5786a
Emma Juliette Brown <60446806+emmajbrown@users.noreply.github.com>
2020-07-12 15:34:44 -0600

    Create End.java

---
+++ src/LinearDataStructures/MinHeap/Workspaces/3/End.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+public class MinHeap {
+    public ArrayList<Integer> heap;
+    public int size;
+
+    public MinHeap() {
+        this.heap = new ArrayList<Integer>();
+        this.heap.add(null);
+        this.size = 0;
+    }
+
+    // Define add() below
+    public void add(int value) {
+        this.heap.add(value);
+        this.size++;
+        System.out.println("Adding " + value);
+        System.out.println(this.heap);
+        this.bubbleUp();
+    }
+
+    // Define bubbleUp() below
+    private void bubbleUp() {
+        System.out.println("Restoring heap properties...");
+    }
+
+    public static void main(String[]args) {
+        MinHeap minHeap = new MinHeap();
+        System.out.println(minHeap.heap);
+
+        // Add 42 to minHeap using add()
+        minHeap.add(42);
+    }
+}
